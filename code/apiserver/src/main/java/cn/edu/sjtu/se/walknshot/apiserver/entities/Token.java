9d4d46e214561bece65a73557204994735011a4a
Zheng Lv <lv.zheng.2015@gmail.com>
2017-06-30 15:11:00 +0800

    apiserver: add hibernate and implement login & token

---
+++ code/apiserver/src/main/java/cn/edu/sjtu/se/walknshot/apiserver/entities/Token.java
@@ -0,0 +1,31 @@
+package cn.edu.sjtu.se.walknshot.apiserver.entities;
+
+public class Token {
+    private int id;
+    private int userId;
+    private String passphrase;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getPassphrase() {
+        return passphrase;
+    }
+
+    public void setPassphrase(String passphrase) {
+        this.passphrase = passphrase;
+    }
+}
---
+++ code/apiserver/src/main/java/cn/edu/sjtu/se/walknshot/apiserver/services/ServiceBase.java
@@ -0,0 +1,8 @@
+package cn.edu.sjtu.se.walknshot.apiserver.services;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class ServiceBase {
+    protected final SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+}
